12eecb685e232fcb487f72083adff4643e46dfb8
Stanislav <euphoo86@gmail.com>
2025-11-26 18:29:34 +0300

    Update webpack.config.cjs

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -33,7 +33,11 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/,
-                type: "asset/resource",
+                type: "asset/inline",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/inline",
             },
         ],
     },
